6a4b2d32a8405c1633fcc547ed1161d2204d15c9
Alexandre Medeiros de Oliveira <Alexandrez322@gmail.com>
2023-05-29 23:40:03 -0300

    minor changes

--- easycrmdocs/docusaurus.config.js
+++ easycrmdocs/docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Alexandre Medeiros', // Usually your GitHub org/user name.
+  projectName: 'Documentation sample', // Usually your repo name.
   themeConfig:{
   },
 
